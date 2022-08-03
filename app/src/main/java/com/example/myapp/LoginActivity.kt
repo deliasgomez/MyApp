6b17ba88fc43d1a1b50397f63f0a879166c17944
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-08-03 15:49:58 -0500

    the splash Screen was implemented in our app

--- app/src/main/java/com/example/myapp/LoginActivity.kt
+++ app/src/main/java/com/example/myapp/LoginActivity.kt
@@ -3,9 +3,13 @@ package com.example.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.LoginTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
     }
 }
