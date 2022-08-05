6bc3383798d538a7b8f2c053d8dd7887952d5523
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-08-05 16:13:18 -0500

    The login screen was implemented in our app

--- app/src/main/java/com/example/myapp/SplashActivity.kt
+++ app/src/main/java/com/example/myapp/SplashActivity.kt
@@ -1,12 +1,12 @@
 package com.example.myapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class LoginActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
